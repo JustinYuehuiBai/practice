81836fe259f2d0f9822d81c52aa8ea427f8786e2
Justin Bai <jybai@usc.edu>
2021-01-20 13:46:04 -0700

    message

---
+++ hello_world/hello_world/hello_world.cpp
@@ -0,0 +1,8 @@
+//
+//  hello_world.cpp
+//  hello_world
+//
+//  Created by Justin Bai on 1/20/21.
+//
+
+#include <stdio.h>
---
+++ hello_world/hello_world/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello_world
+//
+//  Created by Justin Bai on 1/20/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
